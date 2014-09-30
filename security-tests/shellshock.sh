358703d5f2b6cc7b6549fbb096a3f5a1d4dbd045
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-09-30 11:06:18 +0200

    Add CVE-2014-7169 check

--- security-tests/shellshock.sh
+++ security-tests/shellshock.sh
@@ -15,29 +15,56 @@ function check_bash_version {
 	fi
 }
 
-function test_shell_shock {
+#CVE-2014-6271
+function test_shell_shock_cve_2014_6271 {
 
 	#Source of this command: http://thehackernews.com/2014/09/bash-shell-vulnerability-shellshock.html
 	env X="() { :;} ; echo VULNERABILITY" $( which bash ) -c "echo BASH"
 }
 
+#CVE-2014-7169
+function test_shell_shock_cve_2014_7169 {
+
+	cd /tmp; rm -f echo; env 'x=() { (a)=>\' bash -c "echo date"
+
+	if [[ -f /tmp/echo ]]
+	then
+		rm -f /tmp/echo
+		return 1
+	fi
+
+	return 0
+}
+
 function check_shell_shock {
+
+	typeset check_return=0
 
 	echo ""
 	echo "Testing bash bug/Shell Shock vulnerability"
-	typeset shell_shock=$( test_shell_shock 2>/dev/null )
+	typeset shell_shock=$( test_shell_shock_cve_2014_6271 2>/dev/null )
 	if [[ "${shell_shock}" = "BASH" ]]
 	then
-		echo -e "\033[32mThis system is not vulnerable to bash bug/Shell shock\033[0m"
-		return 0
+		echo -e "\033[32mThis system is not vulnerable to shell shock CVE-2014-6271\033[0m"
 	else
-		echo -e "\033[31mThis system is vulnerable to bash bug/Shell shock, BASH must be updated with a fixed version\033[0m"
-		return 1
+		echo -e "\033[31mThis system is vulnerable to shell shock CVE-2014-6271, BASH must be updated with a fixed version\033[0m"
+		check_return=1
 	fi
+
+	test_shell_shock_cve_2014_7169 >/dev/null 2>&1
+	if [[ "${?}" -ne 1 ]]
+	then
+		echo -e "\033[32mThis system is not vulnerable to shell shock CVE-2014-7169\033[0m"
+	else
+		echo -e "\033[31mThis system is vulnerable to shell shock CVE-2014-7169, BASH must be updated with a fixed version\033[0m"
+		check_return=1
+	fi
+
+	return "${check_return}"
 }
 
 #Print the used BASH and the version
 check_bash_version
 #Test bash bug/shell shock command injection
 check_shell_shock
-return $?
+exit $?
