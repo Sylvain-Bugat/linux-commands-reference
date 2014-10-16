daeb6a2f491762c346bc541541be7dc3776cd143
Sylvain Bugat <sylvain.bugat@laposte.net>
2014-10-16 23:54:15 +0200

    Add connection checking

--- security-tests/poodle.sh
+++ security-tests/poodle.sh
@@ -3,7 +3,17 @@
 function test_connection {
 
 	echo "Testing connection to ${1:-localhost}:${2:-443}"
-	typeset connection=$( $echo "" | telnet ${1:-localhost} ${2:-443} )
+	typeset result=$( echo quit | timeout 5 telnet ${1:-localhost} ${2:-443} 2>&1 )
+	typeset connected=$( echo "${result}" | grep -i "connected" )
+	
+	if [[ -z "${connected}" ]]
+	then
+		echo -e "\033[31mCannot connect to the server ${1:-localhost} on port ${2:-443}, check the network\033[0m"
+		return 1
+	else
+		echo -e "\033[32mConnection with server ${1:-localhost} on port ${2:-443} established\033[0m"
+		return 0
+	fi
 }
 
 #CVE-2014-3566
@@ -16,10 +26,19 @@ function test_poodle_cve_2014_3566 {
 	if [[ -z "${certificateFound}" ]]
 	then
 		echo -e "\033[32mThe server ${1:-localhost}:${2:-443} system is not vulnerable to poodle CVE-2014-3566\033[0m"
+		return 0
 	else 
 		echo -e "\033[31mThe server ${1:-localhost}:${2:-443} system is vulnerable to poodle CVE-2014-3566, SSLv3 must be disabled on this server\033[0m"
+		return 1
 	fi
 }
+
+#test server connection
+test_connection $1 $2
+if [[ $? -ne 0 ]]
+then
+	exit 1
+fi
 
 #Test poodle
 test_poodle_cve_2014_3566 $1 $2
