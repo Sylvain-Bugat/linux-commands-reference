cad880c1e454813edcd30dcbf7de8c128d958a7e
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-01-18 16:39:51 +0100

    Add squeeze repeats to a directory

---
+++ file-transformations/squeeze-repeats/squeeze-repeats.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#Check arguments
+if [[ "${#}" -ge 1 && "${#1}" -ne 1 ]]
+then
+	echo "Usage: $0 <character to squeeze>"
+	exit 2
+fi
+
+#Source commands library
+. ../../common-libs/commands-lib.sh
+
+#Character to squeeze, default: space
+typeset -r squeezeCharacter="${1:- }"
+
+#make a reference file based on the most common command
+tr -s "'${squeezeCharacter}'" < "${sourceFile}" > "${refTargetFile}"
+
+#Test all commands
+testCommands "${squeezeCharacter}"
