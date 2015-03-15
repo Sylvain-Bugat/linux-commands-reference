c7fbe8c96aa18abe0bf43b48dd79186021c6976c
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-03-15 19:26:21 +0100

    Add script variants

--- file-processing/count-lines/count-lines.sh
+++ file-processing/count-lines/count-lines.sh
@@ -15,3 +15,46 @@ wc -l < "${sourceFile}" > "${refTargetFile}"
 
 #Test all commands
 testCommands "${selectedLines}"
+
+#Shell scripted variant to count lines
+script="while read line ... lineNumber++ ... done < input-file.txt"
+echo -n "${EXECUTING} ${script}"
+typeset -i lineNumber=0
+while read line
+do
+	(( lineNumber ++ ))
+done < "${sourceFile}"
+echo "${lineNumber}" > "${targetFile}"
+checkGeneratedFile "${script}" "${refTargetFile}" "${targetFile}"
+
+#Shell scripted variant to count lines
+script="while read line < &3 ... lineNumber++ ... done"
+echo -n "${EXECUTING} ${script}"
+typeset -i lineNumber=0
+exec 3< "${sourceFile}"
+while read line <&3
+do
+	(( lineNumber ++ ))
+done
+exec 3<&- 
+echo "${lineNumber}" > "${targetFile}"
+checkGeneratedFile "${script}" "${refTargetFile}" "${targetFile}"
+
+#Shell scripted variant to count lines
+script="while true ... read line <&3 ... done"
+echo -n "${EXECUTING} ${script}"
+typeset -i lineNumber=0
+exec 3< "${sourceFile}"
+> "${targetFile}"
+while true
+do
+	read line <&3
+	if [[ "${?}" -ne 0 ]]
+	then
+		break
+	fi
+	(( lineNumber ++ ))
+done
+exec 3<&-
+echo "${lineNumber}" > "${targetFile}"
+checkGeneratedFile "${script}" "${refTargetFile}" "${targetFile}"
