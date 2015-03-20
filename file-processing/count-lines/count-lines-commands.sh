89427c1c02f9fac77606c8c08ed8f07bca72fde5
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-03-20 21:44:48 +0100

    Add cat | sed/awk/grep/egrep/perl variants

--- file-processing/count-lines/count-lines-commands.sh
+++ file-processing/count-lines/count-lines-commands.sh
@@ -19,17 +19,25 @@ wc < "${sourceFile}"  | awk '{print $1}'
 cat "${sourceFile}" | wc -l
 sed -n '$=' "${sourceFile}"
 sed -n '$=' < "${sourceFile}"
+cat "${sourceFile}" | sed -n '$='
 awk 'END {print NR}' "${sourceFile}"
 awk 'END {print NR}' < "${sourceFile}"
+cat "${sourceFile}" | awk 'END {print NR}'
 grep -c "" "${sourceFile}"
 grep -c "" < "${sourceFile}"
+cat "${sourceFile}" | grep -c ""
 grep --count "" "${sourceFile}"
 grep --count "" < "${sourceFile}"
+cat "${sourceFile}" | grep --count ""
 egrep -c "" "${sourceFile}"
 egrep -c "" < "${sourceFile}"
+cat "${sourceFile}" | egrep -c ""
 egrep --count "" "${sourceFile}"
 egrep --count "" < "${sourceFile}"
+cat "${sourceFile}" | egrep --count ""
 perl -lne 'END { print $. }' "${sourceFile}"
 perl -lne 'END { print $. }' < "${sourceFile}"
+cat "${sourceFile}" | perl -lne 'END { print $. }'
 perl -lne'END { print $. }' "${sourceFile}"
 perl -lne'END { print $. }' < "${sourceFile}"
+cat "${sourceFile}" | perl -lne'END { print $. }'
