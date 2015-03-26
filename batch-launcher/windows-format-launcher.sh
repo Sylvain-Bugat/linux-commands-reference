d5b978fef673c0fb11ae532787fe42079b14f3f4
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-03-26 22:54:52 +0100

    Add windows format test launcher

---
+++ batch-launcher/windows-format-launcher.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "This script cannot be executed because there is of \r character after the shebang. In this case, the exepected executable file is '/bin/bash\r' bu it don't exists!"
