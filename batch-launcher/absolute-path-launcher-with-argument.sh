c547eebb25073debf9011ae928f63ec3a684a756
Sylvain Bugat <sylvain.bugat@laposte.net>
2015-03-28 23:55:49 +0100

    Add batch launcher with argument

---
+++ batch-launcher/absolute-path-launcher-with-argument.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -x
+
+echo "$( basename ${0} ) can be executed with the -x (debug) option, /bin/bash has been found on this system and used to execute this script"
+echo "bash version:"
+/bin/bash --version
